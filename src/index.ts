3e814b178565ded8f6cfd6e200a8b656575a9ead
skd404 <330136168+skd404@users.noreply.github.com>
2026-09-17 23:00:50 +0300

    feat: initilize Express server with health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('HeatPumpHub operational')
+import express, {Request, Response} from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use(cors()).use(express.json())
+
+app.get('/api/v1/health', (req: Request, res: Response) =>{
+    res.status(200).json({
+        message: 'HeatPumpHub backend operational'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port${PORT} in ${process.env.NODE_ENV} mode!`)
+})
