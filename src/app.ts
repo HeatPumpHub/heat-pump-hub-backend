2d14edb8e2dbdf25abf9495e6f5bdd0c4746e92c
skd404 <330136168+skd404@users.noreply.github.com>
2026-09-18 23:30:50 +0300

    feat: implement global error handler with AppError and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/index.js'
+import { errorHandler } from './middleware/error.middleware.js'
 
 const app = express()
 
-app.use(cors()).use(express.json())
+app.use(cors())
+
+app.use(express.json())
 
 app.use('/api/v1', routes)
+
+app.use(errorHandler)
 
 export default app
---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/app.error.js";
+
+export const errorHandler = (
+    err: Error | AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    const statusCode = err instanceof AppError ? err.statusCode : 500;
+    const message = err.message || 'Internal server error'
+
+    res.status(statusCode).json({
+        status: statusCode >= 500 ? 'error' : 'fault',
+        statusCode,
+        message,
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    });
+};
