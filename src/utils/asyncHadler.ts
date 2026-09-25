c398ad8da0fe4a7d7e5382ebd37bdb9c3db5850d
skd404 <330136168+skd404@users.noreply.github.com>
2026-09-26 00:32:41 +0300

    feat: implement AsyncController type for function input parameter in asyncHandler

--- src/utils/asyncHadler.ts
+++ src/utils/asyncHadler.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 
-export const asyncHandler = (fn: RequestHandler): RequestHandler => {
+type AsyncController = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => Promise<any>
+
+export const asyncHandler = (fn: AsyncController) => {
     return (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(fn(req, res, next)).catch(next)
     }
